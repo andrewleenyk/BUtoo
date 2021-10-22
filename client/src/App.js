86763df80def76c8040c3c750bb460d85a2a5502
Andrew Lee <leeandrew@usa.com>
2021-10-22 12:27:59 -0400

    App.js: nav bar created.

--- client/src/App.js
+++ client/src/App.js
@@ -5,8 +5,19 @@ function App() {
   return (
     <div>
       <nav>
-        
+        <div className="home-icon">
+          <h1>BU Speaks</h1>
+          <h5>Boston University</h5>
+        </div>
+        <div className="nav-links">
+          <a>Why</a>
+          <a>Survivor Support</a>
+          <a>Prevention</a>
+          <a>Share Your Story</a>
+          <a>Sign the Petition</a>
+        </div>
       </nav>
+      <hr></hr>
       <Home />
     </div>
   );
