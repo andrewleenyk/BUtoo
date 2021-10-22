008f4a13cf9fd1de50acb18365628b5ec564c550
Andrew Lee <leeandrew@usa.com>
2021-10-22 12:07:53 -0400

    imported axios in Home.js and created usestate for response. Useeffect in progress.

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from './Components/Home.js'
+
 
 function App() {
   return (
--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -1,4 +1,14 @@
-
+import { useEffect } from "react";
+import axios from "axios";
 
 const API_KEY = 'keyN5KW3EITtcAGls'
 const API_URL = `https://api.airtable.com/v0/appix77dwjzsTAHgi/Table%201?api_key=${API_KEY}`
+
+const Home = () => {
+
+    const [response, setResponse] = useState([]);
+
+    useEffect(() => {
+        console.log('getting responses');
+    })
+}
