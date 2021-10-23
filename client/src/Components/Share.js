94d9f1024059819af489614de38ae8e083eb13e6
Andrew Lee <leeandrew@usa.com>
2021-10-23 03:09:26 -0400

    Petition.js component created.

--- client/src/Components/Share.js
+++ client/src/Components/Share.js
@@ -1,9 +1,7 @@
 
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import axios from "axios";
 
-const API_KEY = 'keyN5KW3EITtcAGls'
-const API_URL = `https://api.airtable.com/v0/appix77dwjzsTAHgi/Table%201?api_key=${API_KEY}`
 
 const Share = ({toggleFetch, setToggleFetch}) => {
     const API_KEY = 'keyN5KW3EITtcAGls'
