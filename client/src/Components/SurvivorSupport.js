309c7e3d9f73431b8f5fc755d33c137f8759737c
Andrew Lee <leeandrew@usa.com>
2021-10-25 00:17:19 -0400

    App.css: added gradient animation to nav links when hovered.

--- client/src/Components/SurvivorSupport.js
+++ client/src/Components/SurvivorSupport.js
@@ -2,7 +2,11 @@
 const SurvivorSupport = () => {
 
     return (
-        <h1>Survivor Support</h1>
+
+        <div>
+            <h1>Survivor Support</h1>
+            <iframe src="https://giphy.com/embed/jiH6cmHs9WuOAz8wke" width="480" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
+        </div>
     )
 }
 export default SurvivorSupport;
