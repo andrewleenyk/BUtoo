c36d44383cf013803119f0933268e8fdc9583242
Andrew Lee <leeandrew@usa.com>
2021-10-28 23:56:22 -0400

    Added content to Why and Prevention components.

--- src/Components/Prevention.js
+++ src/Components/Prevention.js
@@ -2,7 +2,18 @@
 const Prevention = () => {
 
     return (
-        <h1>Prevention</h1>
+
+        <div>
+            <h1>Prevention</h1>
+            <ul>
+                <li>Know your resources. Who should you contact if you or a friend needs help? Where should you go? Locate resources such as the campus health center, campus police station, and a local sexual assault service provider. Notice where emergency phones are located on campus, and program the campus security number into your cell phone for easy access.</li>
+                <li>Stay alert. When you’re moving around on campus or in the surrounding neighborhood, be aware of your surroundings. Consider inviting a friend to join you or asking campus security for an escort. If you’re alone, only use headphones in one ear to stay aware of your surroundings.</li>
+                <li>Be careful about posting your location. Many social media sites, like Facebook and Foursquare, use geolocation to publicly share your location. Consider disabling this function and reviewing other social media settings.</li>
+                <li>Make others earn your trust. A college environment can foster a false sense of security. They may feel like fast friends, but give people time to earn your trust before relying on them.</li>
+                <li>Think about Plan B. Spend some time thinking about back-up plans for potentially sticky situations. If your phone dies, do you have a few numbers memorized to get help? Do you have emergency cash in case you can’t use a credit card? Do you have the address to your dorm or college memorized? If you drive, is there a spare key hidden, gas in your car, and a set of jumper cables?</li>
+                <li>Be secure. Lock your door and windows when you’re asleep and when you leave the room. If people constantly prop open the main door to the dorm or apartment, tell security or a trusted authority figure.</li>
+            </ul>
+        </div>
     )
 }
 export default Prevention;
--- src/Components/Why.js
+++ src/Components/Why.js
@@ -7,6 +7,11 @@ const Why = () => {
             <p>
             Boston University should be a safe haven for survivors, not perpetrators. Our purpose is to tell students’ stories of sexual violence at BU to raise awareness of the issue and to push for greater administrative responsibility.
             </p>
+            <h1>About #BUtoo</h1>
+            <p>
+            The magnitude of the #MeToo campaign has created an opportunity to reopen the often-hushed conversation about sexual assaults on college campuses.
+            The conservative estimate is that 1 in 5 American college students will be assaulted. On our own campus, there were 57 cases of sexual offenses reported between 2014 and 2016 alone, and there are many cases that Boston University—despite having detailed policies and resources—has failed to deal with adequately
+            </p>
         </div>
 
     )
